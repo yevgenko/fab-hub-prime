34eb5e8a252ffd5ef3a2e062587499b39945b7d6
Yevgeniy A. Viktorov <yeevgen@gmail.com>
2011-04-11 01:55:11 +0300

    add prime_init task prototype

--- fab_hub_prime/system.py
+++ fab_hub_prime/system.py
@@ -1,5 +1,5 @@
 import os.path
-from fabric.api import env, local, run, cd
+from fabric.api import env, local, run, cd, put
 from fabric.contrib import files
 
 def ssh_add_key(pub_key_file):
@@ -17,11 +17,40 @@ def hub_init(remote='live'):
   #TODO: check if remote exists and can be overwritten or expanded
   #TODO: check if path exists, warn user and use it
   conf = env.conf[remote]
+  """
+  Milestones:
+  * init bare repo
+  * upload post-update hook template
+  * add remote
+  """
   run('mkdir -p %s' % conf['REPO_DIR'])
   with cd(conf['REPO_DIR']):
     run('git --bare init')
-    #put(os.path.join(os.path.split(__file__)[0], 'hook_templates/git/post-update'), 'hooks')
+    #TODO: find template dir
     files.upload_template('post-update', 'hooks/post-update', conf, True,
         template_dir=os.path.join(os.path.split(__file__)[0], 'hook_templates/git'))
     run('chmod +x hooks/post-update')
   local('git remote add %s ssh://%s%s' % (remote, env.host_string, conf['REPO_DIR']))
+
+def prime_init(remote='live', commit=False):
+  conf = env.conf[remote]
+  """
+  Milestones:
+  * init site repo
+  * add remote to hub
+  * optional: add and commit and push to hub
+  * upload post-commit hook template
+  """
+  run('mkdir -p %s' % conf['SITE_DIR'])
+  with cd(conf['SITE_DIR']):
+    run('git init')
+    run('git remote add hub %s' % conf['REPO_DIR'])
+    if commit:
+      put(os.path.join(os.path.split(__file__)[0], 'hook_templates/git/gitignore'), '.gitignore')
+      run('git add .')
+      run('git commit -m "first commit"')
+      run('git push hub master')
+    #TODO: find template dir
+    files.upload_template('post-commit', '.git/hooks/post-commit', conf, True,
+        template_dir=os.path.join(os.path.split(__file__)[0], 'hook_templates/git'))
+    run('chmod +x .git/hooks/post-commit')
